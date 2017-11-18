5deff847bde9a8683b41095442008d0491643d8f
Diego Sebastian <33383251+DiegoSebastian@users.noreply.github.com>
2017-11-17 21:08:37 -0400

    Primera subida

---
+++ code.js
@@ -0,0 +1,15 @@
+let mediaquery = window.matchMedia("(max-width: 800px)");
+if (mediaquery.matches) {
+   let header;
+} else {
+
+   window.onload = function Header() {
+      let header = document.getElementById("header");
+      header.onmousemove = function(e) {
+         let x = -(e.clientX / 15 );
+         let y = -(e.clientY / 15 + 500);
+         header.style.backgroundPosition = x + 'px ' + y + 'px';
+      }
+   }
+}
+
